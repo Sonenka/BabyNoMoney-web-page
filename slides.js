18a890d81112f7573781b82faa6d488336e1506c
Sonya <sonenkkaa@gmail.com>
2025-03-15 04:27:04 +0300

    download video covers

--- slides.js
+++ slides.js
@@ -138,117 +138,117 @@ export const videos = [
         title: "nursery"
     },
     {
-        image: "https://img.youtube.com/vi/GAHuRfJAYCA/maxresdefault.jpg",
+        image: "img/covers/videos/bad to the bone.jpg",
         video: "https://www.youtube.com/embed/GAHuRfJAYCA?si=AjD9UvcgMJF_1xBS",
         title: "bad to the bone"
     },
     {
-        image: "https://img.youtube.com/vi/ppF08KhVXIk/maxresdefault.jpg",
+        image: "img/covers/videos/i remember.jpg",
         video: "https://www.youtube.com/embed/ppF08KhVXIk?si=trIwllqR14MubodS",
         title: "i remember"
     },
     {
-        image: "https://img.youtube.com/vi/lhUlKGmb1VY/maxresdefault.jpg",
+        image: "img/covers/videos/wussup.jpg",
         video: "https://www.youtube.com/embed/lhUlKGmb1VY?si=0McfelrYo0cBW8Uo",
-        title: "wussup "
+        title: "wussup"
     },
     {
-        image: "https://img.youtube.com/vi/6T5FuvkHgzY/maxresdefault.jpg",
+        image: "img/covers/videos/mememe.jpg",
         video: "https://www.youtube.com/embed/6T5FuvkHgzY?si=LOSJRVVHryJCOmgy",
-        title: "mememe "
+        title: "mememe"
     },
     {
-        image: "https://img.youtube.com/vi/8hl3juSJB5w/maxresdefault.jpg",
+        image: "img/covers/videos/sorry.jpg",
         video: "https://www.youtube.com/embed/8hl3juSJB5w?si=7j-gacASlwJyFDRQ",
         title: "sorry"
     },
     {
-        image: "https://img.youtube.com/vi/mpHvHGMZ0jc/maxresdefault.jpg",
+        image: "img/covers/videos/Welcome to Chilis.jpg",
         video: "https://www.youtube.com/embed/mpHvHGMZ0jc?si=SUK_NPCHYPn7jXpd",
         title: "Welcome to Chilis"
     },
     {
-        image: "https://img.youtube.com/vi/Osxpc6ZH0FA/maxresdefault.jpg",
+        image: "img/covers/videos/imma.jpg",
         video: "https://www.youtube.com/embed/Osxpc6ZH0FA?si=QHZ5BLKRYzyiCxnz",
         title: "imma"
     },
     {
-        image: "https://img.youtube.com/vi/9s_HrcAjRj8/maxresdefault.jpg",
+        image: "img/covers/videos/bad thoughts.jpg",
         video: "https://www.youtube.com/embed/9s_HrcAjRj8?si=gbBgo-J16-jnvZlO",
         title: "bad thoughts"
     },
     {
-        image: "https://img.youtube.com/vi/ItqP57SEZJw/maxresdefault.jpg",
+        image: "img/covers/videos/gone.jpg",
         video: "https://www.youtube.com/embed/ItqP57SEZJw?si=y_d_w8HpTyJxt2YG",
         title: "gone"
     },
     {
-        image: "https://img.youtube.com/vi/XKogSBe3XnA/maxresdefault.jpg",
+        image: "img/covers/videos/what would baby do.jpg",
         video: "https://www.youtube.com/embed/XKogSBe3XnA?si=9URRrvoUHos3TaYV",
         title: "what would baby do?"
     },
     {
-        image: "https://img.youtube.com/vi/4LMu5oLrMTo/maxresdefault.jpg",
+        image: "img/covers/videos/man in the mirror.jpg",
         video: "https://www.youtube.com/embed/4LMu5oLrMTo?si=G2ltYmroUaSiPiX5",
         title: "man in the mirror"
     },
     {
-        image: "https://img.youtube.com/vi/wTelhUrRiYE/maxresdefault.jpg",
+        image: "img/covers/videos/shining on my ex.jpg",
         video: "https://www.youtube.com/embed/wTelhUrRiYE?si=9KH6e64cVqMps56f",
         title: "shining on my ex"
     },
     {
-        image: "https://img.youtube.com/vi/Vpht6DXHKbE/maxresdefault.jpg",
+        image: "img/covers/videos/out of control.jpg",
         video: "https://www.youtube.com/embed/Vpht6DXHKbE?si=9aXdDGofC6ZqQzFy",
         title: "out of control"
     },
     {
-        image: "https://img.youtube.com/vi/BgT8OOKr8N4/maxresdefault.jpg",
+        image: "img/covers/videos/moves.jpg",
         video: "https://www.youtube.com/embed/BgT8OOKr8N4?si=ZBkg35NqnEAC90Y5",
         title: "moves"
     },
     {
-        image: "https://img.youtube.com/vi/dmsMGeim1So/maxresdefault.jpg",
+        image: "img/covers/videos/who dat boi.jpg",
         video: "https://www.youtube.com/embed/dmsMGeim1So?si=NZfnhH8EjgW_gNUk",
         title: "who dat boi"
     },
     {
-        image: "https://img.youtube.com/vi/IhU9uvB1Cko/maxresdefault.jpg",
+        image: "img/covers/videos/shining.jpg",
         video: "https://www.youtube.com/embed/IhU9uvB1Cko?si=pIBm_7LONof2pZlN",
         title: "shining"
     },
     {
-        image: "https://img.youtube.com/vi/YzXHL_IIKNk/maxresdefault.jpg",
+        image: "img/covers/videos/moneytalk.jpg",
         video: "https://www.youtube.com/embed/YzXHL_IIKNk?si=l1XOo7ErpZ6qy3nn",
         title: "moneytalk"
     },
     {
-        image: "https://img.youtube.com/vi/MWXdpxgbndw/maxresdefault.jpg",
+        image: "img/covers/videos/take a trip.jpg",
         video: "https://www.youtube.com/embed/MWXdpxgbndw?si=oVWVPfpvlU9Z3Q6w",
         title: "take a trip"
     },
     {
-        image: "https://img.youtube.com/vi/vMx6Y_w_Ox0/maxresdefault.jpg",
+        image: "img/covers/videos/sophisticated.jpg",
         video: "https://www.youtube.com/embed/vMx6Y_w_Ox0?si=6RUOpX_feK_qFAqq",
         title: "sophisticated"
     },
     {
-        image: "https://img.youtube.com/vi/LXyV5hKqA98/maxresdefault.jpg",
+        image: "img/covers/videos/i see london i see france.jpg",
         video: "https://www.youtube.com/embed/LXyV5hKqA98?si=2TU4IrKf1Z1WKgOz",
         title: "i see london i see france"
     },
     {
-        image: "https://img.youtube.com/vi/LiXYi-_MVa4/maxresdefault.jpg",
+        image: "img/covers/videos/two.jpg",
         video: "https://www.youtube.com/embed/LiXYi-_MVa4?si=nbJzRr1Jk2R4VtJT",
         title: "two"
     },
     {
-        image: "https://img.youtube.com/vi/iSyX-aGYHYk/maxresdefault.jpg",
+        image: "img/covers/videos/check.jpg",
         video: "https://www.youtube.com/embed/iSyX-aGYHYk?si=aG6QjoplJ1TDGimD",
         title: "check"
     },
     {
-        image: "https://img.youtube.com/vi/FmlGo1KY2jE/maxresdefault.jpg",
+        image: "img/covers/videos/antidepressants.jpg",
         video: "https://www.youtube.com/embed/FmlGo1KY2jE?si=QC2i4LtNVgkCXvKY",
         title: "antidepressants "
     }
